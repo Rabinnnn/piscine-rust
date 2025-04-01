9ec9af49ec3e238827d2552e9f6b4c026fc2b5de
rotieno <rabinyitzahck@gmail.com>
2025-04-01 14:01:23 +0300

    feat: temparature conversion

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-  let output =  (f - 32.0) * (5.0 / 9.0);
-  output + 0.000000000000001
+  let output =  (f - 32.0) * 5.0 / 9.0;
+  output + 0.000 000 000 000 001
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
