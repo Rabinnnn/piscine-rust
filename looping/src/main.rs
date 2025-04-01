730a802bb1ad0a9ada3ce519dc89c86ef3f4fba8
rotieno <rabinyitzahck@gmail.com>
2025-04-01 20:00:21 +0300

    feat: riddle

---
+++ looping/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+
+fn main() {
+    let riddle = "I am the beginning of the end, and the end of time and space. \
+    I am essential to creation, and I surround every place. What am I?";
+    let correct_answer = "the letter e";
+    let mut attempts = 0;
+
+    loop {
+        println!("{}", riddle);
+        
+        let mut user_input = String::new();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read input");
+        
+        let user_input = user_input.trim().to_lowercase();
+        attempts += 1;
+        
+        if user_input == correct_answer {
+            println!("Number of trials: {}", attempts);
+            break;
+        } else {
+            println!("Incorrect. Try again.\n");
+        }
+    }
+}
---
+++ looping/target/debug/looping.d
@@ -0,0 +1,1 @@
+C:\Users\Rabin\Desktop\piscine-rust\looping\target\debug\looping.exe: C:\Users\Rabin\Desktop\piscine-rust\looping\src\main.rs
