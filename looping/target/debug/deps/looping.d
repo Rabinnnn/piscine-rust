745635238b0e01774522a53192d92ecc739f0e5c
rotieno <rabinyitzahck@gmail.com>
2025-04-01 20:09:00 +0300

    chore: add gitignore

---
+++ looping/target/debug/deps/looping.d
@@ -0,0 +1,5 @@
+C:\Users\Rabin\Desktop\piscine-rust\looping\target\debug\deps\looping.exe: src\main.rs
+
+C:\Users\Rabin\Desktop\piscine-rust\looping\target\debug\deps\looping.d: src\main.rs
+
+src\main.rs:
