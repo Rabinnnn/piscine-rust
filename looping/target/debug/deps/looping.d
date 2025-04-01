40bb0dd90361f0e1f46dcdca3c58b60f5b5b1107
rotieno <rabinyitzahck@gmail.com>
2025-04-01 20:12:35 +0300

    Removed target directory and Cargo.lock from repository

--- looping/target/debug/deps/looping.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Rabin\Desktop\piscine-rust\looping\target\debug\deps\looping.exe: src\main.rs
-
-C:\Users\Rabin\Desktop\piscine-rust\looping\target\debug\deps\looping.d: src\main.rs
-
-src\main.rs:
--- looping/target/debug/looping.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Rabin\Desktop\piscine-rust\looping\target\debug\looping.exe: C:\Users\Rabin\Desktop\piscine-rust\looping\src\main.rs
