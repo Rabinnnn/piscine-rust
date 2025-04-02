5fcf3497245ec0160cc4f2199883c9f3741b1fea
rotieno <rabinyitzahck@gmail.com>
2025-04-02 10:48:09 +0300

    feat: implement matrix transposition

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, PartialEq, Eq)]
-pub struct Matrix((i32, i32), (i32, i32));
+pub struct Matrix(pub (i32, i32), pub (i32, i32));  // ✅ Public fields
 
 pub fn transpose(m: Matrix) -> Matrix {
     Matrix((m.0 .0, m.1 .0), (m.0 .1, m.1 .1))
