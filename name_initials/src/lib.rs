7c0a7bfc09522ea5271f4d5ce6ce7e1c6b1e7f86
rotieno <rabinyitzahck@gmail.com>
2025-04-02 14:56:01 +0300

    manipulate vectors

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -3,8 +3,10 @@ pub fn initials(names: Vec<&str>) -> Vec<String> {
         .iter()
         .map(|name| {
             name.split_whitespace()
-                .filter_map(|word| word.chars().next())
-                .collect() 
+                .filter_map(|word| word.chars().next()) // Get first letter of each word
+                .map(|c| format!("{}.", c)) // Format each letter as "X."
+                .collect::<Vec<_>>() // Collect into a vector
+                .join(" ") // Join with spaces, e.g., "H. P."
         })
-        .collect() 
+        .collect()
 }
