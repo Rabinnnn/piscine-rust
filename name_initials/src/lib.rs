d6b9aa1a0e0f34813fbc90780b069e8c241e3eaf
rotieno <rabinyitzahck@gmail.com>
2025-04-03 10:13:04 +0300

    feat: name initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,12 +1,11 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
-    names
-        .iter()
+    names.iter()
         .map(|name| {
             name.split_whitespace()
-                .filter_map(|word| word.chars().next()) // Get first letter of each word
-                .map(|c| format!("{}.", c)) // Format each letter as "X."
-                .collect::<Vec<_>>() // Collect into a vector
-                .join(" ") // Join with spaces, e.g., "H. P."
+                .filter_map(|word| word.chars().next())
+                .map(|c| format!("{}.", c.to_uppercase()))
+                .collect::<Vec<String>>()
+                .join(" ")
         })
         .collect()
 }
