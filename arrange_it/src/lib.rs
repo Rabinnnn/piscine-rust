f178bd9350f18c723fd4c1efed236f048bee2fe5
rotieno <rabinyitzahck@gmail.com>
2025-04-03 19:57:03 +0300

    function that takes a string literal, sorts the words and returns it. Each word will contain a number that indicates the position of that word.

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub fn arrange_phrase(phrase: &str) -> String {
+    let mut words: Vec<&str> = phrase.split_whitespace().collect();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    words.sort_by_key(|word| {
+        word.chars()
+            .find(|c| c.is_digit(10))
+            .and_then(|c| c.to_digit(10))
+            .unwrap_or(0)
+    });
+    words
+        .iter()
+        .map(|&word| {
+            let removed = word.chars().filter(|c| c.is_alphabetic()).collect();
+            removed
+        })
+        .collect::<Vec<String>>()
+        .join(" ")
 }
