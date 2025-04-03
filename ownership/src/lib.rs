f23bd3379c542934fc2907123c70f14a418c9d0d
rotieno <rabinyitzahck@gmail.com>
2025-04-03 10:29:39 +0300

    feat: ownership

---
+++ ownership/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn first_subword(mut s: String) -> String {
+    let mut chars = s.chars();
+    let mut result = String::new();
+    
+    if let Some(first) = chars.next() {
+        result.push(first);
+    }
+    
+    for c in chars {
+        if c.is_uppercase() || c == '_' {
+            break;
+        }
+        result.push(c);
+    }
+    
+    result
+}
