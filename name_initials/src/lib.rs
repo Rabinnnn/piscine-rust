63752096c93f5143c966a0950d70b83f95530bb3
rotieno <rabinyitzahck@gmail.com>
2025-04-03 10:19:18 +0300

    feat: name initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,11 +1,17 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
     names.iter()
         .map(|name| {
-            name.split_whitespace()
-                .filter_map(|word| word.chars().next())
-                .map(|c| format!("{}.", c.to_uppercase()))
-                .collect::<Vec<String>>()
-                .join(" ")
+            let mut result = String::new();
+            for word in name.split_whitespace() {
+                if let Some(c) = word.chars().next() {
+                    if !result.is_empty() {
+                        result.push(' ');
+                    }
+                    result.push(c.to_ascii_uppercase());
+                    result.push('.');
+                }
+            }
+            result
         })
         .collect()
 }
