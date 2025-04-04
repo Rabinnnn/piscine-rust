8337ae10f589d27fa8aa10d7ac9b0971c548cef0
rotieno <rabinyitzahck@gmail.com>
2025-04-04 19:25:09 +0300

    fix: use tuple struct

--- circle/src/lib.rs
+++ circle/src/lib.rs
@@ -1,15 +1,11 @@
 use std::f64::consts::PI;
 
 #[derive(Debug, Clone, Copy)]
-pub struct Point {
-    pub x: f64,
-    pub y: f64,
-}
+pub struct Point(pub f64, pub f64);
 
 impl Point {
-    // Returns the Euclidean distance between two points
     pub fn distance(&self, other: &Point) -> f64 {
-        ((self.x - other.x).powi(2) + (self.y - other.y).powi(2)).sqrt()
+        ((self.0 - other.0).powi(2) + (self.1 - other.1).powi(2)).sqrt()
     }
 }
 
@@ -20,27 +16,23 @@ pub struct Circle {
 }
 
 impl Circle {
-    // Associated function to create a new circle from coordinates and radius
     pub fn new(x: f64, y: f64, radius: f64) -> Self {
         Circle {
-            center: Point { x, y },
+            center: Point(x, y),
             radius,
         }
     }
 
-    // Returns the diameter of the circle
     pub fn diameter(&self) -> f64 {
         self.radius * 2.0
     }
 
-    // Returns the area of the circle
     pub fn area(&self) -> f64 {
         PI * self.radius.powi(2)
     }
 
-    // Returns true if the circles intersect (touching or overlapping)
     pub fn intersect(&self, other: &Circle) -> bool {
-        let distance = self.center.distance(&other.center);
-        distance <= (self.radius + other.radius)
+        let dist = self.center.distance(&other.center);
+        dist <= self.radius + other.radius
     }
 }
