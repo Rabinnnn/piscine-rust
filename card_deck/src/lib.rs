67ec229d98a92b86fb0ee05373e86bb5cec56a47
rotieno <rabinyitzahck@gmail.com>
2025-04-04 20:27:31 +0300

    feat: card_deck

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -8,61 +8,77 @@ pub enum Suit {
     Club,
 }
 
-impl Suit {
-    // Returns a random Suit
-    pub fn random() -> Suit {
-        let value = rand::thread_rng().gen_range(1..=4);
-        Suit::translate(value)
-    }
-
-    // Translates a u8 into a Suit (1-4)
-    pub fn translate(value: u8) -> Suit {
-        match value {
-            1 => Suit::Heart,
-            2 => Suit::Diamond,
-            3 => Suit::Spade,
-            4 => Suit::Club,
-            _ => panic!("Invalid suit value: must be between 1 and 4"),
-        }
-    }
-}
-
 #[derive(Debug, PartialEq)]
 pub enum Rank {
     Ace,
-    Number(u8), // 2 to 10
+    Number(u8),
     Jack,
     Queen,
     King,
 }
 
-impl Rank {
-    // Returns a random Rank
-    pub fn random() -> Rank {
-        let value = rand::thread_rng().gen_range(1..=13);
-        Rank::translate(value)
-    }
+impl Suit {
+	pub fn random() -> Suit {
+        let mut rng = rand::thread_rng();
+        /* match rng.gen::<u8>() % 4 + 1 {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => unreachable!(),
+        } */
+        Suit::translate(rng.gen::<u8>() % 4 + 1)
+        
+	}
 
-    // Translates a u8 into a Rank (1-13)
-    pub fn translate(value: u8) -> Rank {
+	pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => unreachable!(),
+        }
+	}
+}
+
+impl Rank {
+	pub fn random() -> Rank {
+        let mut rng = rand::thread_rng();
+        /* match rng.gen::<u8>() % 13 + 1{
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(rng.gen::<u8>() % 10 + 1),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => unreachable!(),
+        } */
+        Rank::translate(rng.gen::<u8>() % 13 + 1)
+        
+	}
+
+	pub fn translate(value: u8) -> Rank {
         match value {
             1 => Rank::Ace,
             2..=10 => Rank::Number(value),
             11 => Rank::Jack,
             12 => Rank::Queen,
             13 => Rank::King,
-            _ => panic!("Invalid rank value: must be between 1 and 13"),
+            _ => unreachable!(),
         }
-    }
+	}
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Card {
-    pub suit: Suit,
-    pub rank: Rank,
+	pub suit: Suit,
+	pub rank: Rank,
 }
 
-// Returns true if the card is Ace of Spades
 pub fn winner_card(card: &Card) -> bool {
-    card.suit == Suit::Spade && card.rank == Rank::Ace
+    match (&card.suit, &card.rank) {
+        (Suit::Spade, Rank::Ace) => true,
+        _ => false,
+    }
+    
 }
